0af11dce4f4c30f3cf9be0e18f15646a3e5a6f19
VeryDirtyBoy <128145074+VeryDirtyBoy@users.noreply.github.com>
2023-03-17 18:11:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice_2._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите драгоценности (J): ");
+            string J = Console.ReadLine();
+            Console.WriteLine("Введите камни (S): ");
+            string S = Console.ReadLine();
+
+            var jArray = J.ToCharArray();
+            var sArray = S.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < jArray.Length; i++)
+            {
+                for (int j = 0; j < sArray.Length; j++)
+                {
+                    if (sArray[j] == jArray[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Кол-во символов из S входящие в J: {count}");
+
+        }
+    }
+}
