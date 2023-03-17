54ee826bf3ca1587b534b656a9d264b3e8c2c0a9
VeryDirtyBoy <128145074+VeryDirtyBoy@users.noreply.github.com>
2023-03-17 18:42:23 +0700

    Add files via upload

---
+++ 2.6/task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://api.openweathermap.org/data/2.5/weather?lat=56.4977&lon=84.9744&appid=80b6bbb076aa77c84b58573deeabb9cc";
+        using var client = new HttpClient();
+        var array = await client.GetAsync(url);
+        string b = await array.Content.ReadAsStringAsync();
+        SearchResult c = JsonConvert.DeserializeObject<SearchResult>(b);
+        Console.WriteLine($"Погода в Томске: {1.8 * (c.Main.temp - 273)}");
+    }
+    public class SearchResult
+    {
+        public Temp Main { get; set; }
+    }
+    public class Temp
+    {
+        public double temp { get; set; }
+    }
+}
